2f049f51644fd234651b8d77f26be2f5d24a585f
T-Szymk <ziggys_42@hotmail.com>
2021-09-11 22:31:27 +0300

    added tb skeleton for the vga_colr_gen

---
+++ tb/vga_colr_gen_tb.vhd
@@ -0,0 +1,32 @@
+-------------------------------------------------------------------------------
+-- Title      : VGA Colour Generator Testbench
+-- Project    : VGA Controller
+--------------------------------------------------------------------------------
+-- File       : vga_colr_gen_tb.vhd
+-- Author(s)  : Thomas Szymkowiak
+-- Company    : TUNI
+-- Created    : 2021-09-11
+-- Design     : vga_colr_gen_tb
+-- Platform   : -
+-- Standard   : VHDL'08
+--------------------------------------------------------------------------------
+-- Description: Testbench for VGA Colour Generator
+--
+--------------------------------------------------------------------------------
+-- Revisions:
+-- Date        Version  Author  Description
+-- 2021-09-11  1.0      TZS     Created
+--------------------------------------------------------------------------------
+LIBRARY IEEE;
+USE IEEE.STD_LOGIC_1164.ALL;
+
+ENTITY vga_colr_gen_tb IS 
+END ENTITY vga_colr_gen_tb;
+
+--------------------------------------------------------------------------------
+
+ARCHITECTURE tb of vga_colr_gen_tb IS 
+BEGIN 
+
+END ARCHITECTURE tb;
+--------------------------------------------------------------------------------
