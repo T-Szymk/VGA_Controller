01c38eb95c736e3e179ce4f061eb9e73d508022a
T-Szymk <ziggys_42@hotmail.com>
2022-07-31 17:23:05 +0300

    comment vga_sim app code

--- supporting_apps/vga_sim/vga_sim_server.py
+++ supporting_apps/vga_sim/vga_sim_server.py
@@ -2,7 +2,7 @@
 import socket
 import pygame
 
-DEBUG = False
+DEBUG = False  # turns on DEBUG prints
 
 # socket constants
 HOST = "127.0.0.1"
@@ -11,9 +11,11 @@ BUFF_SIZE = 2048
 FRAME_LIMIT = 255
 
 
-class vga_sim_window():
+# Manages all aspects of the VGA simulator display window
+class VGASimWindow:
 
-    def __init__(self, width, height):
+    def __init__(self, width=640, height=480):
+
         self.background_colour = (0, 0, 0)
         self.height = height
         self.width = width
@@ -24,28 +26,33 @@ class vga_sim_window():
         self.end_sim_msg = self.font.render("Close Window", True, (255, 255, 255))
         # initialise screen object
         self.screen = pygame.display.set_mode((width, height))
-        # Set the caption of the screen
+        # Set the initial caption of the screen
         pygame.display.set_caption('VGA Simulator')
         # Fill the background colour to the screen
         self.screen.fill(self.background_colour)
 
-    def update_display(self):
+    @staticmethod
+    def update_display():
         pygame.display.update()
 
+    # display black screen
     def blank_screen(self):
         self.screen.fill(self.background_colour)
         self.update_display()
 
+    # display no connection caption on screen
     def display_no_connection(self):
         self.screen.fill(self.background_colour)
         self.screen.blit(self.no_connection_msg, ((self.width / 2) - 150, (self.height / 2) - 30))
         self.update_display()
 
+    # display caption to indicate there is not client connection to server
     def display_close_window(self):
         self.screen.fill(self.background_colour)
         self.screen.blit(self.end_sim_msg, ((self.width / 2) - 150, (self.height / 2) - 30))
         self.update_display()
 
+    # set value of pixel on the screen
     def set_pxl(self, x, y, color):
         self.screen.set_at((x, y), color)
 
@@ -54,6 +61,7 @@ class vga_sim_window():
 def process_data(data, line, vga_sim_obj):
     line_arr = []
     for pxl_id in range(0, vga_sim_obj.width):
+        # read 3 bytes out of buffer (each byte represents a single colour)
         tmp_pxl = int.from_bytes(data[(3 * pxl_id):(3 * pxl_id) + 3], "little")
         b = tmp_pxl & 255
         g = (tmp_pxl >> 8) & 255
@@ -63,10 +71,11 @@ def process_data(data, line, vga_sim_obj):
 
 
 def main():
-    vga_sim = vga_sim_window(640, 480)
-
+    # create simulation window
+    vga_sim = VGASimWindow(640, 480)
+    # flag to indicate simulator window is running
     running = True
-
+    # While waiting for client, indicate no connection
     vga_sim.display_no_connection()
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
@@ -76,7 +85,7 @@ def main():
         conn, addr = sock.accept()
 
         with conn:
-
+            # once connected, remove no connection caption
             vga_sim.blank_screen()
 
             # Variable to keep our game loop running
@@ -84,20 +93,21 @@ def main():
             line_id = 0
 
             while True:
-
+                # enter infinite loop to wait for data, receive data, process data (repeat)
                 data = conn.recv(BUFF_SIZE)
 
                 if data != b'':
 
                     if DEBUG:
                         print(f"Received: {data}")
-
-                    if data[0:5] == b'RESET': # if reset string received, clear screen and return to top
+                    # check for reset event and clear screen if detected to mimic hardware response
+                    if data[0:5] == b'RESET':
 
                         line_id = 0
                         vga_sim.blank_screen()
 
                     else:
+                        # read data from received packet and update on simulator window
                         process_data(data, line_id, vga_sim)
 
                         if line_id == (vga_sim.height - 1):
